d3a3b732c34dc5c4fc37f04fed29a9fa03c71465
Sean <lettersforsean@yahoo.co.jp>
2016-02-25 10:03:37 +1100

    Change trianglify colours.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -9,9 +9,13 @@ window.onload = function() {
           width: dimensions.width,
           height: dimensions.height,
           variance: 0.9,
-          // x_colors: ['#550000','#550000', '#fff'],
-          x_colors: 'Reds',
-          y_colors: 'RdBu'
+          // x_colors: ['#550000','#801515', '#fff'],
+          // x_colors: ['#550000','#fa2f43', '#6966b1'],
+          x_colors: ['#550000','#fa2f43', '#fff'],
+          // x_colors: ['#fff', '#fa2f43', '#550000']
+          // y_colors: ['#550000','#9b0000', '#5882a3'],
+          // x_colors: 'Reds',
+          // y_colors: 'RdBu'
       });
       target.style['background-image'] = 'url(' + pattern.png() + ')';
   }
