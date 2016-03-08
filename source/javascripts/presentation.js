9955e8be0e2ab79d3ee575cb3038b281b3a8c366
Sean <lettersforsean@yahoo.co.jp>
2016-03-08 22:17:41 +1100

    Get presentation pages to take up whole viewport.

--- source/javascripts/presentation.js
+++ source/javascripts/presentation.js
@@ -7,13 +7,8 @@ window.onload = function() {
           width: dimensions.width,
           height: dimensions.height,
           variance: 0.9,
-          // x_colors: ['#550000','#801515', '#fff'],
-          // x_colors: ['#550000','#fa2f43', '#6966b1'],
-          x_colors: ['#550000','#fa2f43', '#fff'],
-          // x_colors: ['#fff', '#fa2f43', '#550000']
-          // y_colors: ['#550000','#9b0000', '#5882a3'],
-          // x_colors: 'Reds',
-          // y_colors: 'RdBu'
+          x_colors: 'RdYlGn',
+          y_colors: 'Spectral'
       });
       target.style['background-image'] = 'url(' + pattern.png() + ')';
   }
