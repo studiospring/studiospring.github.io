58a13dcf4cfec23d04279ae49ccac2d71745b23f
Sean <lettersforsean@yahoo.co.jp>
2016-07-01 11:39:35 +1000

    Remove unused js.

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -5,14 +5,3 @@ var wow = new WOW({
     offset:       100
 });
 wow.init();
-
-//Contact Form
-
-$('#submit').click(function(){
-
-$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
- $('#success').html(response);
-});
-return false;
-
-});
