36a35d2f0c3685d30085daf0c2eb4ec539b5dde5
Sean <lettersforsean@yahoo.co.jp>
2016-07-29 15:59:09 +1000

    Scroll event kinda working.

--- source/javascripts/portfolio/main.js
+++ source/javascripts/portfolio/main.js
@@ -2,6 +2,24 @@ window.onload = function() {
   // Accordion
   $('.collapse').collapse()
 
+  // $(document).scroll(function() {
+  //   event.preventDefault();
+  // });
+
+  var lastScrollTop = $(window).scrollTop()
+  $(window).scroll(function(event){
+    event.preventDefault();
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+      // downscroll code
+      alert('scroll down');
+    } else {
+      // upscroll code
+      alert('scroll up');
+    }
+    lastScrollTop = st;
+  });
+
   var waypoint = new Waypoint({
     element: $('#personas'),
     handler: function() {
