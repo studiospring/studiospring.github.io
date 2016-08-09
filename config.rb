dabe9705c628ec657ff337f643ddb3770e58d2a6
Sean <lettersforsean@yahoo.co.jp>
2016-08-09 21:04:29 +1000

    Add year to article path.

--- config.rb
+++ config.rb
@@ -30,7 +30,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "{title}.html"
+  blog.permalink = "{year}/{title}.html"
   # Matcher for blog source files
   blog.sources = "{year}/{title}.html"
   # blog.taglink = "tags/{tag}.html"
